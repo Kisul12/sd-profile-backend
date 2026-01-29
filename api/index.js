be9e745cdbd71568752ea9719367c302911a662d
Kisul12 <134827926+Kisul12@users.noreply.github.com>
2026-01-30 03:34:36 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,10 +1,6 @@
 const connectDB = require('../src/config/database')
 const app = require('../src/app')
 
-console.log('🚀 Function invoked')
-
 connectDB()
-    .then(() => console.log('✅ MongoDB connected'))
-    .catch((err) => console.error('❌ MongoDB error:', err))
 
 module.exports = app
