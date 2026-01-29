8c440d2cc72b0c0369a97ca241f4477effd845e4
Kisul12 <134827926+Kisul12@users.noreply.github.com>
2026-01-30 02:34:24 +0800

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+    if (cached.conn) return cached.conn
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+    if (!cached.promise) {
+        cached.promise = mongoose
+            .connect(process.env.MONGO_URI, {
+                bufferCommands: false
+            })
+            .then((mongoose) => mongoose)
     }
-};
 
-module.exports = connectDB;
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+module.exports = connectDB
